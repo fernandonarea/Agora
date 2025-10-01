cbb31ef3aba833af7bf4f2c442bb344f23bc96a7
Fernando <fernandonarea6@gmail.com>
2025-10-01 17:20:14 -0500

    User provider en el lugar que corresponde

--- frontend/src/layout/MainLayout.jsx
+++ frontend/src/layout/MainLayout.jsx
@@ -1,15 +1,18 @@
-import {Outlet, Link} from 'react-router-dom'
-import SideBar from '../components/sideBar/side-bar';
+import { Outlet, Link } from "react-router-dom";
+import SideBar from "../components/sideBar/side-bar";
+import { UserProvider } from "@/context/userContext";
 
 export default function MainLayout() {
   return (
-    <div className='flex flex-row max-h-dvh min-h-dvh'>
-      <aside>
-        <SideBar/>
-      </aside>
-      <main className="flex flex-col w-full max-h-screen overflow-auto">
-        <Outlet />
-      </main>
-    </div>
+    <UserProvider>
+      <div className="flex flex-row max-h-dvh min-h-dvh">
+        <aside>
+          <SideBar />
+        </aside>
+        <main className="flex flex-col w-full max-h-screen overflow-auto">
+          <Outlet />
+        </main>
+      </div>
+    </UserProvider>
   );
 }
